e8d7b090559b16d538e19a6c86538e741c5b930c
nullgr4vity <nullgravity@nullgravitys-MacBook-Air.local>
2016-09-14 14:44:27 +0200

    add new tests for events handlers

--- test/components/Table.js
+++ test/components/Table.js
@@ -55,4 +55,74 @@ describe('<Table>', () => {
     assert.equal(rows.length, 20); // 2 (cols) x 10 (rows)
   });
 
+  it('Should capture onAddRow event', (done) => {
+    let onClick = () => {
+      done();
+    };
+
+    let instance = ReactTestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <Table tools={true} onAddRow={onClick}/>
+      </Provider>
+    );
+
+    const btns = ReactTestUtils.scryRenderedDOMComponentsWithTag(instance, 'button');
+    // there is 1 + 2 * rows buttons in whole component
+    // btns[0] => 'add new item' button
+    ReactTestUtils.Simulate.click(btns[0]); 
+  });
+
+  it('Should capture onEditRow event', (done) => {
+    let onClick = (rowId) => {
+      expect(rowId).equal(0);
+      done();
+    };
+
+    let instance = ReactTestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <Table tools={true} onEditRow={onClick}/>
+      </Provider>
+    );
+
+    const btns = ReactTestUtils.scryRenderedDOMComponentsWithTag(instance, 'button');
+    // there is 1 + 2 * rows buttons in whole component
+    // btns[1] => first row, trash button
+    ReactTestUtils.Simulate.click(btns[1]); 
+  });
+
+  it('Should capture onDeleteRow event', (done) => {
+    let onClick = (rowId) => {
+      expect(rowId).equal(0);
+      done();
+    };
+
+    let instance = ReactTestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <Table tools={true} onDeleteRow={onClick}/>
+      </Provider>
+    );
+
+    const btns = ReactTestUtils.scryRenderedDOMComponentsWithTag(instance, 'button');
+    // there is 1 + 2 * rows buttons in whole component
+    // btns[2] => first row, trash button
+    ReactTestUtils.Simulate.click(btns[2]); 
+  });
+
+  it('Should capture onSelectRow event', (done) => {
+    let onClick = (rowId) => {
+      expect(rowId).equal(5);
+      done();
+    };
+
+    let instance = ReactTestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <Table tools={true} onSelectRow={onClick}/>
+      </Provider>
+    );
+
+    const rows = ReactTestUtils.scryRenderedDOMComponentsWithTag(instance, 'tr');
+    // no header provided to the table so row 5 is 5
+    ReactTestUtils.Simulate.click(rows[5]); 
+  });    
+
 });
